97ac72c124ba288ef3489e2c68e9478d0304d9a1
hmcgough <hugh.mcgough@wsu.edu>
2018-09-13 15:25:27 -0700

    Add strlib to linker instructions

--- mk.sh
+++ mk.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 arm-none-eabi-as -mcpu=arm926ej-s -g ts.s -o ts.o
 arm-none-eabi-gcc -c -mcpu=arm926ej-s -g t.c -o t.o
-arm-none-eabi-ld -T t.ld ts.o t.o -o t.elf
+arm-none-eabi-ld -T t.ld ts.o t.o strlib -o t.elf
 arm-none-eabi-objcopy -O binary t.elf t.bin
 
 rm *.o *.elf
